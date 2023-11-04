1d4489318bcfeb061cf671a62a4ada41ee4a48ad
eren <cevdet.eren.2001@gmail.com>
2023-11-04 18:10:01 +0300

    uygulama 3

---
+++ Uygulama_3/main.py
@@ -0,0 +1,27 @@
+import requests
+
+# Weatherstack API anahtarını buraya ekleyin
+API_KEY = '2217bf4dfeeda7780b2c7ea56e62bc4a'
+
+def hava_durumu_sorgula(sehir):
+    base_url = "http://api.weatherstack.com/current"
+    complete_url = f"{base_url}?access_key={API_KEY}&query={sehir}"
+
+    response = requests.get(complete_url)
+    #burada bir kod parçası eksik 
+    data = response.json()
+    if "current" in data:
+        current_data = data["current"]
+        sıcaklık = current_data["temperature"]
+        nem_orani = current_data["humidity"]
+        hava_durumu = current_data["weather_descriptions"][0]
+
+        print(f"Hava Durumu: {hava_durumu}")
+        print(f"Sıcaklık: {sıcaklık} °C")
+        print(f"Nem Oranı: {nem_orani}%")
+    else:
+        print("Şehir bulunamadı veya hava durumu bilgileri alınamadı.")
+
+if __name__ == "__main__":
+    sehir = input("Hava durumu bilgisini almak istediğiniz şehri girin: ")
+    hava_durumu_sorgula(sehir)
